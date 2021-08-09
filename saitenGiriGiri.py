eeb8176cfb6d6fec3c2299a7b803006738448a3c
PhysKen <62343651+phys-ken@users.noreply.github.com>
2021-08-10 00:11:04 +0900

    解答用紙を出力

--- saitenGiriGiri.py
+++ saitenGiriGiri.py
@@ -1,7 +1,7 @@
 import tkinter
 from tkinter import messagebox
 from tkinter.constants import TRUE
-from PIL import Image, ImageTk  # 外部ライブラリ
+from PIL import Image, ImageTk, ImageDraw, ImageFont  # 外部ライブラリ
 import csv
 import glob
 import os
@@ -920,6 +920,95 @@ def output_Sh():
     wb.save(RESULT_FILE_NAME)
 
 
+def writeImg():
+    def readCSV_loc():
+        # もしcsvが無ければ、全部止める
+        if os.path.isfile("./setting/trimData.csv") == False:
+            return 0
+        else:
+            with open('./setting/trimData.csv') as f:
+                reader = csv.reader(f)
+                data = [row for row in reader]
+                data.pop(0)
+                return data
+
+    def getPoint(figName, qName):
+        qCol = int(qName[-4:]) + 3
+        xlPath = "./setting/saiten.xlsx"
+        wb = openpyxl.load_workbook(xlPath)
+        ws = wb["採点シート"]
+        list = [[0 for i in range(ws.max_column)] for j in range(ws.max_row)]
+        for x in range(0, ws.max_row):
+            for y in range(0, ws.max_column):
+                list[x][y] = ws.cell(row=x+1, column=y+1).value
+        for row in list:
+            if figName in row:
+                result = True
+                break
+        result
+
+        if not str(row[qCol]) == "未":
+            return str(row[qCol])
+        else:
+            return "?"
+
+    files = glob.glob("./setting/input/*")
+    data = readCSV_loc()
+    img = Image.open(files[0])
+    draw = ImageDraw.Draw(img)  # ImageDrawオブジェクトを作成
+
+    if data:
+        try:
+            shutil.rmtree("./setting/kaitoYousi")
+        except:
+            print("kaitoYousiフォルダがまだありません、今から作ります。")
+        os.makedirs("./setting/kaitoYousi", exist_ok=True)
+        nmtitle, nmleft, nmtop, nmright, nmbottom = data.pop(0)
+        h = abs(int(nmbottom) - int(nmtop))
+        w = abs(int(nmright) - int(nmleft))
+        if nmtitle == "name":
+            if h >= w:
+                mojiSize = int(w/2)
+            else:
+                mojiSize = int(h/2)
+            font = ImageFont.truetype("arial.ttf", int(
+                mojiSize))  # フォントを指定、64はサイズでピクセル単位
+
+    for f in files:
+        img = Image.open(f)
+        draw = ImageDraw.Draw(img)  # ImageDrawオブジェクトを作成
+        print(os.path.basename(f) + "を採点します。")
+        sumVal = 0
+        for pos in data:
+            title, left, top, right, bottom = pos
+            figName = os.path.basename(f)
+            qName = title
+
+            text = getPoint(figName, qName)
+            try:
+                sumVal = sumVal + int(text)
+            except:
+                sumVal = sumVal
+
+            # 画像
+            draw.text((int(int(right) - mojiSize/2), int(top)),
+                      text, font=font, fill="red")
+            draw.rectangle((int(int(right) - mojiSize/2), int(top),
+                           int(int(right) + mojiSize/2), int(top) + mojiSize),  outline="red")
+
+        # 合計点を描画
+        text = str(sumVal)
+        draw.text((int(int(nmright) - mojiSize/2), int(nmtop)),
+                  text, font=font, fill="red")
+        draw.rectangle((int(int(nmright) - mojiSize/2), int(nmtop),
+                       int(int(nmright) + mojiSize/2), int(nmtop) + mojiSize),  outline="red")
+        # ファイルを保存
+        img.save('setting/kaitoYousi/' + os.path.basename(f),
+                 quality=100, optimize=True)
+        print(os.path.basename(f) + "の採点は正しく終了しました。")
+    messagebox.showinfo("確認", "採点済みの解答用紙を作成しました。\n./setting/kaitoYousiの中に入っています。")
+
+
 def top_activate():
 
     val = 0.4
@@ -972,6 +1061,9 @@ def top_activate():
 
     outxlsxB = tkinter.Button(
         button_frame, text="Excelに出力", command=outputXlsx, width=botWid, height=2, highlightthickness=0).pack(expand=exBool)
+
+    writeImgB = tkinter.Button(
+        button_frame, text="採点済み画像を出力", command=writeImg, width=botWid, height=2, highlightthickness=0).pack(expand=exBool)
 
     exitB = tkinter.Button(
         button_frame, text="アプリを閉じる", command=exitGiri, width=botWid, height=2, highlightthickness=0).pack(expand=exBool)
